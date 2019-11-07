969d62cf07df220bb36b9d1bb61840007a047691
Maxime CHAMOT <mchamot@adneom.com>
2019-11-07 08:50:56 +0100

    Creating GivingBackChangeContext

---
+++ GivingBackChange/GivingBackChange.Dal/Contexts/GivingBackChangeContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Common;
+using GivingBackChange.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace GivingBackChange.Dal.Contexts
+{
+    public class GivingBackChangeContext : DbContext
+    {
+        private const string ConnectionString = "Data Source=./givingBackChange.db";
+
+        public GivingBackChangeContext()
+        {
+            
+        }
+
+        public DbSet<Coin> Coins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(ConnectionString);
+        }
+    }
+}
