6bf405c690209e3e5260b7332ce50c37be1c4926
Maxime CHAMOT <mchamot@adneom.com>
2019-11-10 19:23:23 +0100

    Fic error in GivingBackChangeContext + Edit db path

--- GivingBackChange/GivingBackChange.Dal/Contexts/GivingBackChangeContext.cs
+++ GivingBackChange/GivingBackChange.Dal/Contexts/GivingBackChangeContext.cs
@@ -1,16 +1,20 @@
-﻿using System.Data.Common;
-using GivingBackChange.Entity;
+﻿using GivingBackChange.Entity;
 using Microsoft.EntityFrameworkCore;
 
 namespace GivingBackChange.Dal.Contexts
 {
     public class GivingBackChangeContext : DbContext
     {
-        private const string ConnectionString = "Data Source=./givingBackChange.db";
+        private static GivingBackChangeContext _instance;
+        private const string ConnectionString = "Data Source='C:\\Program Files\\dotnet\\givingBackChange.db'";
 
-        public GivingBackChangeContext()
+        private GivingBackChangeContext()
         {
-            
+        }
+
+        public static GivingBackChangeContext GetInstance()
+        {
+            return _instance ?? (_instance = new GivingBackChangeContext());
         }
 
         public DbSet<Coin> Coins { get; set; }
