e03d0adb416e659022c0401735c0ff81d742e06b
Maxime CHAMOT <mchamot@adneom.com>
2019-11-10 19:20:56 +0100

    Edit ICoinBoxService

--- GivingBackChange/GivingBackChange.Business/Services/ICoinBoxService.cs
+++ GivingBackChange/GivingBackChange.Business/Services/ICoinBoxService.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using GivingBackChange.Business.BusinessObjects;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using GivingBackChange.Business.BusinessObjects;
 
 namespace GivingBackChange.Business.Services
 {
     public interface ICoinBoxService
     {
         /// <summary>
-        /// Permet d'obtenir la liste des pièces ainsi que leur quantité dans la caisse du distributeur
-        /// de la plus petite valeur jusqu'à la plus grande
+        /// Permet d'obtenir la liste des pièces ainsi que la quantité disponible
+        /// pour chacune des pièces dans la caisse du distributeur
         /// </summary>
         /// <returns>La liste des pièces disponibles avec leur quantité</returns>
-        Task<IList<CoinBo>> GetCoinsOrderedByAscendingOrderValue();
+        Task<IList<CoinBo>> GetCoins();
 
         /// <summary>
-        /// Permet d'obtenir la liste des pièces ainsi que leur quantité dans la caisse du distributeur
-        /// de la plus grande valeur jusqu'à la plus petite
+        /// Permet de mettre à jour un référentiel de pièces
         /// </summary>
-        /// <returns>La liste des pièces disponibles avec leur quantité</returns>
-        Task<IList<CoinBo>> GetCoinsOrderedByDescendingOrderValue();
+        /// <param name="updatedCoins"></param>
+        /// <returns></returns>
+        Task UpdateCoins(IList<CoinBo> updatedCoins);
     }
 }
