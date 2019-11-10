3c313443cf6fd058e61ef8fc2b79f7d8d1b448a1
Maxime CHAMOT <mchamot@adneom.com>
2019-11-10 19:19:37 +0100

    Create IDatabaseService + Register to unity Container

---
+++ GivingBackChange/GivingBackChange.Business/Services/Implementations/DatabaseService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using GivingBackChange.Dal.Managers;
+
+namespace GivingBackChange.Business.Services.Implementations
+{
+    public class DatabaseService : IDatabaseService
+    {
+        private readonly IDatabaseManager _databaseManager;
+
+        public DatabaseService(IDatabaseManager databaseManager)
+        {
+            this._databaseManager = databaseManager;
+        }
+
+        public async Task DeleteAndCreateNewDatabase()
+        {
+            await this._databaseManager.EnsureDeleted();
+            await this._databaseManager.EnsureCreated();
+        }
+
+        public async Task SeedData()
+        {
+            await this._databaseManager.Seed();
+        }
+    }
+}
