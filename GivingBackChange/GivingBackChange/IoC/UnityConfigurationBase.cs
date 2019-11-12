734a0ad517a91d791cb9a241117dc5ed58e4bc0c
Maxime CHAMOT <mchamot@adneom.com>
2019-11-12 14:15:59 +0100

    UnityConfigurationBase created

---
+++ GivingBackChange/GivingBackChange/IoC/UnityConfigurationBase.cs
@@ -0,0 +1,19 @@
+﻿using GivingBackChange.Business.IoC;
+using GivingBackChange.Dal.IoC;
+using Unity;
+
+namespace GivingBackChange.Ui.IoC
+{
+    public static class UnityConfigurationBase
+    {
+        private static IUnityContainer _container;
+
+        public static IUnityContainer Container => _container = _container ?? new UnityContainer();
+
+        public static void RegisterDependencies()
+        {
+            UnityConfigurationBusiness.RegisterDependencies(Container);
+            UnityConfigurationDal.RegisterDependencies(Container);
+        }
+    }
+}
