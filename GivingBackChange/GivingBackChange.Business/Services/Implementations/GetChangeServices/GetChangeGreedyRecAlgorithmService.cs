00042d91012f4cf1d0bac47353cd3d0d99ce977d
Maxime CHAMOT <mchamot@adneom.com>
2019-11-12 13:46:53 +0100

    Fix GetChangeGreedyRecAlgorithmService => UpdateCoin added

--- GivingBackChange/GivingBackChange.Business/Services/Implementations/GetChangeServices/GetChangeGreedyRecAlgorithmService.cs
+++ GivingBackChange/GivingBackChange.Business/Services/Implementations/GetChangeServices/GetChangeGreedyRecAlgorithmService.cs
@@ -1,4 +1,4 @@
-using GivingBackChange.Business.BusinessObjects;
+﻿using GivingBackChange.Business.BusinessObjects;
 using GivingBackChange.Business.Services.Implementations.GetChangeServices.Base;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,25 +18,37 @@ namespace GivingBackChange.Business.Services.Implementations.GetChangeServices
             var change = new List<CoinBo>();
 
             this.GetChangeRec(this.GetRemainingAmountInCents(remaningAmount), 0, coinReferential, change);
+            await this._coinBoxService.UpdateCoins(coinReferential);
 
             return change;
         }
 
-        private void GetChangeRec(float remaningAmountInCents, int i, IList<CoinBo> coinReferential, IList<CoinBo> change)
+        private void GetChangeRec(int remaningAmountInCents, int i, IList<CoinBo> coinReferential, IList<CoinBo> change)
         {
-            if (i == coinReferential.Count)
-            {
-                return;
-            }
-
             if (remaningAmountInCents >= coinReferential[i].ValueInCent && coinReferential[i].Quantity > 0)
             {
-                var time = (int)(remaningAmountInCents / coinReferential[i].ValueInCent);
-                remaningAmountInCents = remaningAmountInCents - (time * coinReferential[i].ValueInCent);
+                // Calculate the number of time I can take the specific coin
+                var time = remaningAmountInCents / coinReferential[i].ValueInCent;
+
+                if (coinReferential[i].Quantity - time < 0)
+                {
+                    time = coinReferential[i].Quantity;
+                }
+
+                // Calculate the remaining amount in cents
+                remaningAmountInCents -= (time * coinReferential[i].ValueInCent);
+
+                // Update coin quantity 
+                coinReferential[i].Quantity -= time;
+
+                // Add in change the coin with the amount
                 change.Add(new CoinBo(coinReferential[i].Id, coinReferential[i].Label, coinReferential[i].Value, time));
             }
 
-            this.GetChangeRec(remaningAmountInCents, i + 1, coinReferential, change);
+            if (i + 1 < coinReferential.Count)
+            {
+                this.GetChangeRec(remaningAmountInCents, i + 1, coinReferential, change);
+            }
         }
     }
 }
